9067da30188ec98480b25cbebe0648ff5a027563
YoshikaUrano <21aw0106@jec.ac.jp>
2022-06-14 11:27:29 +0900

    urano test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ahh</Text>
+      <Text>ah</Text>
       <StatusBar style="auto" />
     </View>
   );
